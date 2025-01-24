d2ce534b05b9626751864cc0c5962df35097ab18
Felipe Coste <32721311+felipecoste@users.noreply.github.com>
2025-01-24 19:09:00 +0000

    modelo terminado

--- src/models.py
+++ src/models.py
@@ -1,32 +1,80 @@
+import enum
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
+    
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(100), nullable=False, unique= True)
+    first_name = Column(String(100), nullable=False)
+    last_name = Column(String(100), nullable=False)
+    email = Column(String(150), nullable=False, unique= True)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    posts = relationship("Post", back_populates="author", cascade="all, delete-orphan")
+    comments = relationship("Comment", back_populates="author", cascade="all, delete-orphan")
+    followers = relationship(
+        "User", secondary="follower", 
+        primaryjoin="User.id==follower.user_from_id", 
+        secondaryjoin="User.id==follower.user_to_id", 
+        back_populates="following"
+    )
+    following = relationship(
+        "User", 
+        secondary="follower", 
+        primaryjoin="User.id==follower.user_to_id", 
+        secondaryjoin="User.id==follower.user_from_id", 
+        back_populates="followers"
+    )
+
+class Follower(Base):
+    __tablename__ = "follower"
+
+    user_from_id = Column(Integer, ForeignKey("user.id"), nullable=False, primary_key=True)
+    user_to_id = Column(Integer, ForeignKey("user.id"), nullable=False, primary_key=True)
+
+class Post(Base):
+    __tablename__ = 'post'
+    
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
 
-    def to_dict(self):
-        return {}
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+    author = relationship("User", back_populates="posts")
+
+    comments = relationship("Comment", back_populates="post", cascade="all, delete-orphan")
+    media = relationship("Media", back_populates="post", cascade="all, delete-orphan")
+
+class Comment(Base):
+    __tablename__ = "comment"
+
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(500), nullable=False)
+
+    author_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+    author = relationship("User", back_populates="comments")
+
+    post_id = Column(Integer, ForeignKey("post.id"), nullable=False)
+    post = relationship("Post", back_populates="comments")
+
+class MediaType(enum.Enum):
+    VIDEO = "video"
+    IMAGE = "image"
+
+class Media(Base):
+    __tablename__= "media"
+
+    id = Column(Integer, primary_key=True)
+    type = Column(Enum(MediaType), nullable=False)
+    ulr= Column(String(300), nullable=False)
+
+    post_id = Column(Integer, ForeignKey("post.id"), nullable=False)
+    post = relationship("Post", back_populates="media")
 
 ## Draw from SQLAlchemy base
 try:
